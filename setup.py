2bb1b8734566a6dac0d38da3b87d39cadc5f910f
Lizeth Lucero <lizethlucero@yahoo.com>
2021-04-15 06:39:56 -0400

    p9 done

--- setup.py
+++ setup.py
@@ -6,21 +6,40 @@ Lizeth Lucero
 import csv
 import math
 
+examples = []
+ys = [] # label
 
 # Load data into python from CSV file
 with open("deidentified_data.csv") as fp:
     rows = csv.reader(fp)
     header = next(rows) # I assume your datasets have a row with column-labels!
     for row in rows:
-        print(header) # print the labels
-        print(row) # print the current row
+        # print(header) # print the labels
+        # print(row) # print the current row
         # entry = dict(zip(header, row)) # glue them into a dict
         # print(entry) # print that dict
-	# want to remove rows that have NaN for ECG or skin tone
-	if (not row[0] == 'NaN') and (not row[8] == 'NaN'):
-		entry = dict(zip(header, row)) # glue them into a dict
-        	break # stop after 1 row of data, so you can inspect, choose which columns matter, etc.
+    
+        # want to remove rows that have NaN for ECG or skin tone
+        if (not row[0] == 'NaN') and (not row[8] == 'NaN'):
+            features = dict(zip(header, row)) # glue them into a dict
+            features.pop('Activity', None)
+            features.pop('ID', None)
+            for key in features.keys():
+                if features[key] == 'NaN':
+                    features.update({key: float(0)})
+                else:
+                    features.update({key: float(features[key])})
+            ECG = features.pop('ECG', None)
+            entry = {'features':features}
+            entry.update({'ECG': ECG})
 
+            
+            keep = entry["features"]
+            ys.append(entry["ECG"])
+            examples.append(keep)
+            
+            # print("entry:", entry) # entry
+            # break # stop after 1 row of data, so you can inspect, choose which columns matter, etc.
 
 '''
 I think that my task definition works well for this project
@@ -35,7 +54,118 @@ I am using those as the 'true' result. Additionally, I will be removing any data
 NaN for skin color because I am hoping to use that for my algorithm. 
 
 
-Data Types:
-
+'''
 
 '''
+Training Set Check In
+
+I will need a non-linear model. First I reformatted all the NaN's in my data to equal 0
+I also looked into non-numerical data and removed activity and ID for now but I might
+put back activity once I transform it into numerical(ordinal) data.
+
+
+Regarding the training of the data, I think I may not have enough data based on the graphs, 
+but I need to check more into this especially since I hope to add activity.
+
+'''
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from shared import simple_boxplot
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import resample
+import json
+from sklearn.tree import DecisionTreeRegressor
+
+#%% load up the data
+# examples = []
+# ys = [] # label
+
+# Load our data to list of examples:
+# with open(dataset_local_path("poetry_id.jsonl")) as fp:
+#     for line in fp:
+#         info = json.loads(line)
+#         keep = info["features"]
+#         ys.append(info["poetry"])
+#         examples.append(keep)
+
+## CONVERT TO MATRIX:
+feature_numbering = DictVectorizer(sort=True, sparse=False)
+X = feature_numbering.fit_transform(examples)
+del examples
+
+## SPLIT DATA:
+RANDOM_SEED = 12345678
+
+# Numpy-arrays are more useful than python's lists.
+y = np.array(ys)
+# split off train/validate (tv) pieces.
+rX_tv, rX_test, y_tv, y_test = train_test_split(
+    X, y, train_size=0.75, shuffle=True, random_state=RANDOM_SEED
+)
+# split off train, validate from (tv) pieces.
+rX_train, rX_vali, y_train, y_vali = train_test_split(
+    rX_tv, y_tv, train_size=0.66, shuffle=True, random_state=RANDOM_SEED
+)
+
+
+scale = StandardScaler()
+X_train = scale.fit_transform(rX_train)
+X_vali: np.ndarray = scale.transform(rX_vali)  # type:ignore
+X_test: np.ndarray = scale.transform(rX_test)  # type:ignore
+
+#%% Actually compute performance for each % of training data
+N = len(y_train)
+num_trials = 100
+percentages = list(range(5, 100, 5))
+percentages.append(100)
+scores = {}
+acc_mean = []
+acc_std = []
+
+
+# Which subset of data will potentially really matter.
+for train_percent in percentages:
+    n_samples = int((train_percent / 100) * N)
+    print("{}% == {} samples...".format(train_percent, n_samples))
+    label = "{}".format(train_percent, n_samples)
+
+    # So we consider num_trials=100 subsamples, and train a model on each.
+    scores[label] = []
+    for i in range(num_trials):
+        X_sample, y_sample = resample(
+            X_train, y_train, n_samples=n_samples, replace=False
+        )  # type:ignore
+        # Note here, I'm using a simple classifier for speed, rather than the best.
+        clf = DecisionTreeRegressor(random_state=RANDOM_SEED + train_percent + i)
+        clf.fit(X_sample, y_sample)
+        # so we get 100 scores per percentage-point.
+        scores[label].append(clf.score(X_vali, y_vali))
+    # We'll first look at a line-plot of the mean:
+    acc_mean.append(np.mean(scores[label]))
+    acc_std.append(np.std(scores[label]))
+
+# First, try a line plot, with shaded variance regions:
+import matplotlib.pyplot as plt
+
+means = np.array(acc_mean)
+std = np.array(acc_std)
+plt.plot(percentages, acc_mean, "o-")
+plt.fill_between(percentages, means - std, means + std, alpha=0.2)
+plt.xlabel("Percent Training Data")
+plt.ylabel("Mean Accuracy")
+plt.xlim([0, 100])
+plt.title("Shaded Accuracy Plot")
+plt.savefig("graphs/p09-area-Accuracy.png")
+plt.show()
+
+
+# Second look at the boxplots in-order: (I like this better, IMO)
+simple_boxplot(
+    scores,
+    "Learning Curve",
+    xlabel="Percent Training Data",
+    ylabel="Accuracy",
+    save="graphs/p09-boxplots-Accuracy.png",
+)
---
+++ shared.py
@@ -0,0 +1,178 @@
+# This 'shared.py' will be imported from in our practicals going forward (sometimes, anyway).
+import os
+import urllib.request
+import sys
+import zipfile
+from typing import List, Dict, Optional, Any
+
+from sklearn.base import ClassifierMixin
+from sklearn.utils import resample
+from sklearn.metrics import accuracy_score, roc_auc_score
+import random
+
+
+def bootstrap_auc(
+    f: Any,  # sklearn classifier
+    X,  # numpy array
+    y,  # numpy array
+    num_samples: int = 100,
+    random_state: int = random.randint(0, 2 ** 32 - 1),
+    truth_label: int = 1,
+) -> List[float]:
+    """
+    Take the classifier ``f``, and compute it's bootstrapped AUC over the dataset ``X``,``y``.
+    Generate ``num_samples`` samples; and seed the resampler with ``random_state``.
+    """
+    dist: List[float] = []
+    if hasattr(f, "decision_function"):
+        y_scores = f.decision_function(X)
+        # type:ignore (predict not on ClassifierMixin)
+    else:
+        y_scores = f.predict_proba(X)[:, truth_label]
+    # do the bootstrap:
+    for trial in range(num_samples):
+        sample_pred, sample_truth = resample(
+            y_scores, y, random_state=trial + random_state
+        )  # type:ignore
+        score = roc_auc_score(y_true=sample_truth, y_score=sample_pred)  # type:ignore
+        dist.append(score)
+    return dist
+
+
+def bootstrap_accuracy(
+    f: ClassifierMixin,
+    X,  # numpy array
+    y,  # numpy array
+    num_samples: int = 100,
+    random_state: int = random.randint(0, 2 ** 32 - 1),
+) -> List[float]:
+    """
+    Take the classifier ``f``, and compute it's bootstrapped accuracy over the dataset ``X``,``y``.
+    Generate ``num_samples`` samples; and seed the resampler with ``random_state``.
+    """
+    dist: List[float] = []
+    y_pred = f.predict(X)  # type:ignore (predict not on ClassifierMixin)
+    # do the bootstrap:
+    for trial in range(num_samples):
+        sample_pred, sample_truth = resample(
+            y_pred, y, random_state=trial + random_state
+        )  # type:ignore
+        score = accuracy_score(y_true=sample_truth, y_pred=sample_pred)  # type:ignore
+        dist.append(score)
+    return dist
+
+
+def TODO(for_what: str) -> None:
+    """Because crashing should be legible."""
+    print("=" * 80)
+    print("TODO:", for_what, file=sys.stderr)
+    print("=" * 80)
+    sys.exit(-1)
+
+
+def __create_data_directory():
+    os.makedirs("data", exist_ok=True)
+    assert os.path.exists("data") and os.path.isdir("data")
+
+
+def __download_file(url: str, path: str):
+    # empty data files were mis-downloaded...
+    if os.path.exists(path) and os.path.getsize(path) > 0:
+        # don't download multiple times.
+        return
+    # try connecting before creating output file...
+    with urllib.request.urlopen(url) as f:
+        # create output file and download the rest.
+        with open(path, "wb") as out:
+            out.write(f.read())
+
+
+def dataset_local_path(name: str) -> str:
+    __create_data_directory()
+    destination = os.path.join("data", name)
+    if name == "AirQualityUCI.csv":
+        zip_path = os.path.join("data", "AirQualityUCI.zip")
+        __download_file(
+            "http://archive.ics.uci.edu/ml/machine-learning-databases/00360/AirQualityUCI.zip",
+            zip_path,
+        )
+        with zipfile.ZipFile(zip_path) as zf:
+            zf.extract(name, "data")
+        return destination
+    if name == "forest-fires.csv":
+        __download_file(
+            "http://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv",
+            destination,
+        )
+    elif name == "poetry_id.jsonl":
+        __download_file(
+            "http://ciir.cs.umass.edu/downloads/poetry/id_datasets.jsonl", destination
+        )
+    elif name in [
+        "lit-wiki-2020.jsonl.gz",
+        "tiny-wiki.jsonl.gz",
+        "tiny-wiki-labels.jsonl",
+    ]:
+        __download_file("http://static.jjfoley.me/{}".format(name), destination)
+    else:
+        raise ValueError("No such dataset... {}; should you git pull?".format(name))
+    assert os.path.exists(destination)
+    return destination
+
+
+def simple_boxplot(
+    data: Dict[str, List[float]],
+    title: Optional[str] = None,
+    xlabel: Optional[str] = None,
+    ylabel: Optional[str] = None,
+    show: bool = True,
+    save: Optional[str] = None,
+) -> Any:
+    """ Create a simple set of named boxplots. """
+    import matplotlib.pyplot as plt
+
+    box_names = []
+    box_dists = []
+    for (k, v) in data.items():
+        box_names.append(k)
+        box_dists.append(v)
+    plt.boxplot(box_dists)
+    plt.xticks(
+        rotation=30,
+        horizontalalignment="right",
+        ticks=range(1, len(box_names) + 1),
+        labels=box_names,
+    )
+    if title:
+        plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+    if ylabel:
+        plt.ylabel(ylabel)
+    plt.tight_layout()
+    if save:
+        plt.savefig(save)
+    if show:
+        plt.show()
+    return plt
+
+
+# # TESTS:
+
+
+# def test_download_poetry():
+#     import json
+
+#     lpath = dataset_local_path("poetry_id.jsonl")
+#     with open(lpath) as fp:
+#         first = json.loads(next(fp))
+#         assert first["book"] == "aceptadaoficialmente00gubirich"
+
+
+# def test_download_wiki():
+#     import json
+
+#     lpath = dataset_local_path("tiny-wiki-labels.jsonl")
+#     with open(lpath) as fp:
+#         first = json.loads(next(fp))
+#         print(first)
